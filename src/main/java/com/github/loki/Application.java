b4733642208de02f9de39f77dd644fba38fa7426
Flavio Andrade Machado <flaviochess@gmail.com>
2018-09-23 22:32:07 -0300

    init project

---
+++ src/main/java/com/github/loki/Application.java
@@ -0,0 +1,18 @@
+package com.github.loki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Flavio Andrade <onsave.com.br>
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+}
