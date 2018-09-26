c97f1d857d425fb313d3c22b81e37ab6d0bbb9f7
Flavio Andrade <flavio.machado@bcash.com.br>
2018-09-26 20:43:23 -0300

    add h2 with data tests and change handler to work with h2 data

--- src/main/java/com/github/loki/response/GetResponse.java
+++ src/main/java/com/github/loki/response/GetResponse.java
@@ -1,34 +1,24 @@
 package com.github.loki.response;
 
-import org.springframework.stereotype.Component;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Flavio Andrade
  */
-@Component //trocar para repositorio
+@Repository
 public class GetResponse {
+    
+    @Autowired
+    private ResponseConfigRepository repository;
     
     public ResponseConfig byPortAndMethodAndUri(Integer port, String method, String uri) {
         
-        if(uri.equalsIgnoreCase("/loki/test")) {
-            
-            return response200(port, method);
-        }
+        Optional<ResponseConfig> responseConfig = repository.findByPortAndMethodAndUri(port, method, uri);
         
-        return response404(port, method);
-    }
-    
-    private ResponseConfig response200(Integer port, String method) {
-        
-        ResponseConfig responseConfig = new ResponseConfig();
-        
-        responseConfig.setPort(port);
-        responseConfig.setMethod(method);
-        responseConfig.setStatusCode(200);
-        responseConfig.setBody("[{\"account\":{\"id\":709918,\"description\":\"Brasil Account\",\"personId\":28333,\"name\":\"Flavio Andrade\",\"model\":\"XPTO\",\"country\":\"BR\",\"enabled\":true},\"profile\":{\"id\":\"ADMIN\",\"name\":\"ADMIN\",\"standard\":true,\"order\":1}}]");
-        
-        return responseConfig;
+        return responseConfig.orElse(response404(port, method));
     }
     
     private ResponseConfig response404(Integer port, String method) {
@@ -38,7 +28,7 @@ public class GetResponse {
         responseConfig.setPort(port);
         responseConfig.setMethod(method);
         responseConfig.setStatusCode(404);
-        responseConfig.setBody("");
+        responseConfig.setBody("{\"status\": 404, \"error\": \"Not Found\", \"message\": \"Not Found\"}");
         
         return responseConfig;
     }
--- src/main/java/com/github/loki/response/ResponseConfig.java
+++ src/main/java/com/github/loki/response/ResponseConfig.java
@@ -1,5 +1,9 @@
 package com.github.loki.response;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +13,12 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-public class ResponseConfig {
+@Entity
+public class ResponseConfig implements Serializable {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
     
     private Integer port;
     
